af2810c8bf1fcc79e2040d9161633fe6907398ca
Natascha <blumernatascha@gmail.com>
2026-05-16 10:39:18 +0200

    Backend added Color, for merge

--- Backend/src/main/java/com/example/demo/mapper/TaskMapper.java
+++ Backend/src/main/java/com/example/demo/mapper/TaskMapper.java
@@ -6,11 +6,14 @@ import com.example.demo.model.TaskModel;
 public class TaskMapper {
 
     public static TaskModel toModel(TaskDTO dto) {
-        return new TaskModel(dto.getTaskdescription());
+        return new TaskModel(dto.getTaskdescription(), dto.getTaskColor);
     }
 
     public static TaskDTO toDTO(TaskModel model) {
         TaskDTO dto = new TaskDTO();
+        if (TaskModel.getColor() == null || TaskModel.getColor().isEmpty()) {
+            TaskModel.setColor("#ffffff");
+        }
         dto.setTaskdescription(model.getTaskdescription());
         return dto;
     }
--- Backend/src/main/java/com/example/demo/models/TaskDTO.java
+++ Backend/src/main/java/com/example/demo/models/TaskDTO.java
@@ -2,9 +2,11 @@ package com.example.demo.dto;
 
 public class TaskDTO {
     private String taskdescription;
-
+    private String color;
     public TaskDTO() {}
 
     public String getTaskdescription() { return taskdescription; }
     public void setTaskdescription(String taskdescription) { this.taskdescription = taskdescription; }
-}
+    public String getColor() { return color; }
+    public void setColor(String color) { this.color = color; }
+    }
