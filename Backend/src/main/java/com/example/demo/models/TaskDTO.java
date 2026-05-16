7790d7bbe3d3a5c3613b15b1ad16c09c533ae4b7
Natascha <blumernatascha@gmail.com>
2026-05-16 12:55:17 +0200

    Fix: Fixed package naming issues

--- Backend/src/main/java/com/example/demo/models/TaskDTO.java
+++ Backend/src/main/java/com/example/demo/models/TaskDTO.java
@@ -1,4 +1,4 @@
-package com.example.demo.dto;
+package com.example.demo.models;
 
 public class TaskDTO {
     private String taskdescription;
--- Backend/src/main/java/com/example/demo/models/TaskModel.java
+++ Backend/src/main/java/com/example/demo/models/TaskModel.java
@@ -1,4 +1,4 @@
-package com.example.demo.model;
+package com.example.demo.models;
 
 public class TaskModel {
     private String taskdescription;
