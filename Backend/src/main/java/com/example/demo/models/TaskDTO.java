90fbf2e64bf23af6413f7ac4dd3fd4775e9a4c87
ElodinDerBarde <r.marchese96@gmail.com>
2026-05-16 10:23:59 +0200

    Model Prepared, model prepared with priority

--- Backend/src/main/java/com/example/demo/models/TaskDTO.java
+++ Backend/src/main/java/com/example/demo/models/TaskDTO.java
@@ -1,13 +1,10 @@
 package com.example.demo.models;
 
-enum Priority {
-    HIGH, MEDIUM, LOW
-}
-public class ToDoModel {
+public class TaskDTO {
     int id;
     String text;
     Priority priority;
-    public ToDoModel(int id, String text, Priority priority) {
+    public TaskDTO(int id, String text, Priority priority) {
         this.id = id;
         this.text = text;
         this.priority = priority;
