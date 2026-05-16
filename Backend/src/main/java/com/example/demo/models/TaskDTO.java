04a50fef771bdfe1ab5caa332d944b6b5aa3b629
ElodinDerBarde <r.marchese96@gmail.com>
2026-05-16 15:15:18 +0200

    s

--- Backend/src/main/java/com/example/demo/models/TaskDTO.java
+++ Backend/src/main/java/com/example/demo/models/TaskDTO.java
@@ -3,14 +3,18 @@ package com.example.demo.models;
 public class TaskDTO {
     private String taskdescription;
     private String color;
-    private Tag tag;
-
+    private EnumPriority priority;
     public TaskDTO() {}
 
     public String getTaskdescription() { return taskdescription; }
     public void setTaskdescription(String taskdescription) { this.taskdescription = taskdescription; }
     public String getColor() { return color; }
     public void setColor(String color) { this.color = color; }
-    public Tag getTag() { return tag; }
-    public void setTag(Tag tag) { this.tag = tag; }
-}
+    public void setPriority(EnumPriority priority) {
+        this.priority = priority;
+    }
+
+    public EnumPriority getPriority() {
+        return priority;
+    }
+    }
