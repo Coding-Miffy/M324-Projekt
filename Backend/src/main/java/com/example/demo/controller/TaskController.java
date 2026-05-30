f18635c9111ac639118f7244c7bbe5b5ac0392ee
ElodinDerBarde <r.marchese96@gmail.com>
2026-05-30 15:54:48 +0200

    fix: add /api/v1 prefix to TaskController

--- Backend/src/main/java/com/example/demo/controller/TaskController.java
+++ Backend/src/main/java/com/example/demo/controller/TaskController.java
@@ -3,12 +3,7 @@ package com.example.demo.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.models.Tag;
 import com.example.demo.models.TaskDTO;
@@ -17,6 +12,7 @@ import com.example.demo.service.TaskService;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api/v1")
 public class TaskController {
 
     private final TaskService taskService;
